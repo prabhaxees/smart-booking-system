3e29148e7350ac96f1233f01a006673f577e6382
prabhaxees <prabhasees2256.be23@chitkara.edu.in>
2026-05-10 17:39:28 +0530

    analytics available for all users

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { getAnalytics } from "../controllers/analyticsController.js";
-import { protect, adminOnly } from "../middleware/authMiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// 👑 Admin only
-router.get("/", protect, adminOnly, getAnalytics);
+// Authenticated users
+router.get("/", protect, getAnalytics);
 
 export default router;
